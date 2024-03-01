27cd570db4e7272cf492c2d3511e0485b7ea4bb0
Hamza <chikhi.h@gmail.com>
2024-02-29 19:06:59 -0500

    fix: uncomment npm install

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# original_dir=$(pwd)
+original_dir=$(pwd)
 
-# cd ./back
-# npm install
-# cd "$original_dir"
+cd ./back
+npm install
+cd "$original_dir"
 
-# cd ./front
-# npm install
-# cd "$original_dir"
+cd ./front
+npm install
+cd "$original_dir"
 
 docker-compose down
 
